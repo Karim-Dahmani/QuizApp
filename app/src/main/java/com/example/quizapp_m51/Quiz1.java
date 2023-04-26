43250269f92e0129c0e2d8ef7db8e7e76363d688
Dahmani Mohammed Karim <109095748+Karim-Dahmani@users.noreply.github.com>
2023-04-27 00:50:38 +0100

    commit 2

--- app/src/main/java/com/example/quizapp_m51/Quiz1.java
+++ app/src/main/java/com/example/quizapp_m51/Quiz1.java
@@ -25,30 +25,6 @@ public class Quiz1 extends AppCompatActivity {
         setContentView(R.layout.activity_quiz1);
         rg=(RadioGroup) findViewById(R.id.rg);
         bNext=(Button) findViewById(R.id.bNext);
-        bNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(rg.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(),"Merci de choisir une réponse S.V.P !",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    rb = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
-                    //Toast.makeText(getApplicationContext(),rb.getText().toString(),Toast.LENGTH_SHORT).show();
-                    if(rb.getText().toString().equals(RepCorrect)){
-                        score+=1;
-                        //Toast.makeText(getApplicationContext(),score+"",Toast.LENGTH_SHORT).show();
-                    }
-                    Intent intent=new Intent(Quiz1.this,Quiz2.class);
-                    intent.putExtra("score",score);
-                    startActivity(intent);
-                    //overridePendingTransition(R.anim.fadein,R.anim.fadeout);
-                   overridePendingTransition(R.anim.exit,R.anim.entry);
-                    finish();
-                }
-
-            }
-        });
 
     }
 }
